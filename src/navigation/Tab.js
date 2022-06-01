119b9104b9fcd06f75363806463f7ac9f85f1857
Matheus Vinicius <mviniciuscardoso546@gmail.com>
2022-06-01 10:58:49 -0300

    resolvendo bug drawer navigation e estudando import de icons

--- src/navigation/Tab.js
+++ src/navigation/Tab.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Ionicons from '@expo/vector-icons/Ionicons';
 import TelaA from '../views/TelaA'
 import TelaB from '../views/TelaB'
 import TelaC from '../views/TelaC'
@@ -7,11 +8,44 @@ import TelaC from '../views/TelaC'
 const Tab = createBottomTabNavigator()
 
 export default props => (
-    <Tab.Navigator screenOptions={{
-        activeTintColor: 'red',
-        inactiveTintColor: 'blue',
-        labelStyle: { fontSize: 30 }
-    }} initialRouteName = 'TelaB'>
+    <Tab.Navigator screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          switch (route.name) {
+            case 'TelaA':
+                iconName = focused
+                    ? 'ios-home'
+                    : 'ios-home-outline';
+                break
+            case 'TelaB':
+                iconName = focused
+                    ? 'ios-information-circle'
+                    : 'ios-information-circle-outline';
+                break
+            case 'TelaC':
+                iconName = focused ? 'ios-list-circle' : 'ios-list';
+                break
+
+          }
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'ios-information-circle'
+              : 'ios-information-circle-outline';
+          } else if (route.name === 'Settings') {
+            iconName = focused ? 'ios-list-box' : 'ios-list';
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'red',
+        tabBarInactiveTintColor: 'blue',
+        tabBarLabelStyle: { fontSize: 22 },
+        tabBarShowLabel: false
+    })}
+    initialRouteName = 'TelaB'>
         <Tab.Screen name = 'TelaA' component={TelaA} />
         <Tab.Screen name = 'TelaB' component={TelaB} />
         <Tab.Screen name = 'TelaC' component={TelaC} />
